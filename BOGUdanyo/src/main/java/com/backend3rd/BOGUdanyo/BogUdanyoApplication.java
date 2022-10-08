9fbce2997fb9063a65d537633e6fccd1120637c9
TenJeong <tenj845@gmail.com>
2022-10-08 17:18:38 +0900

    project generated

---
+++ BOGUdanyo/src/main/java/com/backend3rd/BOGUdanyo/BogUdanyoApplication.java
@@ -0,0 +1,13 @@
+package com.backend3rd.BOGUdanyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogUdanyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BogUdanyoApplication.class, args);
+	}
+
+}
---
+++ BOGUdanyo/src/test/java/com/backend3rd/BOGUdanyo/BogUdanyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend3rd.BOGUdanyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogUdanyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
