3d80c6c72b5f2cb12bc31e201f61400f4272cc1c
parkjiyun <pjyun05200@gmail.com>
2022-11-13 20:12:30 +0900

    update MyAccidentRepository

--- BOGUdanyo/src/main/java/com/backend3rd/BOGUdanyo/repository/MyAccidentRepository.java
+++ BOGUdanyo/src/main/java/com/backend3rd/BOGUdanyo/repository/MyAccidentRepository.java
@@ -4,13 +4,14 @@ import com.backend3rd.BOGUdanyo.entity.AccidentArea;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface MyAccidentRepository extends JpaRepository<AccidentArea, Integer> {
 
-    @Query("select a from AccidentArea a where DEGREES(ACOS(SIN(RADIANS(a.lat))*SIN(RADIANS(:lat)) + COS(RADIANS(a.lat))*COS(RADIANS(:lat))*COS(RADIANS(a.lon-:lon))))* 60*1.1515*1609.344 < :10000")
-    List<AccidentArea> findByRegion(@Param(value = "lat") float lat,
-                                    @Param(value = "lon") float lon);
+    @Query("select a from AccidentArea a where DEGREES(ACOS(SIN(RADIANS(a.lat))*SIN(RADIANS(:startLat)) + COS(RADIANS(a.lat))*COS(RADIANS(:startLat))*COS(RADIANS(a.lon-:startLon))))* 60*1.1515*1609.344 < :10000")
+    List<AccidentArea> findByRegion(@Param(value = "startLat") float startLat,
+                                    @Param(value = "startLon") float startLon);
 
 }
